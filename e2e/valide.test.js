fbdcabea31ac06986973ef0fbc523064211275bb
Yanius27 <zhzh-1987@mail.ru>
2024-02-04 23:41:23 +0700

    testing #6

--- e2e/valide.test.js
+++ e2e/valide.test.js
@@ -3,23 +3,42 @@
  */
 
 import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
-describe('Card Number validate', () => {
-  let browser;
-  let page;
+jest.setTimeout(30000);
 
-  beforeEach(async () => {
-    browser = await puppeteer.launch({
-      headless: false,
-      slowMo: 100,
-      devtools: true,
+describe('Credit Card Validator form', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
     });
 
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      devtools: true,
+    });
     page = await browser.newPage();
   });
 
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
   test('Valid number should change class of all icons except right icon', async () => {
-    await page.goto('http://localhost:9000/');
+    await page.goto(baseUrl);
     await page.waitForSelector('.input_validate');
 
     const input = await page.$('.input_validate');
@@ -29,10 +48,10 @@ describe('Card Number validate', () => {
     await submitBtn.click();
 
     await page.waitForSelector('.icon_masterCard.icon_inactive');
-  }, 25000);
+  }, 30000);
 
   test('Invalid number should not change class of all icons', async () => {
-    await page.goto('http://localhost:9000/');
+    await page.goto(baseUrl);
     await page.waitForSelector('.input_validate');
 
     const input = await page.$('.input_validate');
@@ -42,9 +61,5 @@ describe('Card Number validate', () => {
     await submitBtn.click();
 
     await page.waitForSelector('.icon_masterCard.icon_active');
-  }, 25000);
-
-  afterEach(async () => {
-    await browser.close();
-  });
+  }, 30000);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   module: {
     rules: [
       {
