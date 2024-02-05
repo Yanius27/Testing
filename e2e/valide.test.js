3f4f035c3ebe738961fd6d039ef8c9f84e3b2036
Yanius27 <zhzh-1987@mail.ru>
2024-02-05 14:57:39 +0700

    testing #14

--- e2e/valide.test.js
+++ e2e/valide.test.js
@@ -25,7 +25,7 @@ describe('Credit Card Validator form', () => {
     });
 
     browser = await puppeteer.launch({
-      headless: 'new',
+      headless: false,
       slowMo: 250,
       devtools: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
