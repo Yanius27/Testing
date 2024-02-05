7e530b0b69f78d802f6822b3a52530b8c0b558ce
Yanius27 <zhzh-1987@mail.ru>
2024-02-05 14:16:15 +0700

    testing #11

--- e2e/valide.test.js
+++ e2e/valide.test.js
@@ -25,7 +25,6 @@ describe('Credit Card Validator form', () => {
     });
 
     browser = await puppeteer.launch({
-      headless: false,
       slowMo: 250,
       devtools: true,
     });
--- puppeteerrc.cjs
+++ puppeteerrc.cjs
@@ -1,4 +1,4 @@
-const {join} = require('path');
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
@@ -6,5 +6,5 @@ const {join} = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
 };
